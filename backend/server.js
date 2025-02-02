78d91896c257fdddbef6f2a2b4066e6cffb650a6
Tiffany-Amber Jacobs <105398577+TiffanyAmberJacobs@users.noreply.github.com>
2025-02-02 22:30:59 +0200

    Lots and lots of debugging.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import app from './App.js';
+
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
